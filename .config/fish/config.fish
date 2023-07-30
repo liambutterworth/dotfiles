db5e252e03474a60e1368c1bb1f6ed86987d1885
Liam Butterworth <liambutterworth@randallreilly.com>
2023-07-29 22:17:20 -0500

    update fish config and include functions in repo

---
+++ .config/fish/config.fish
@@ -0,0 +1,98 @@
+#
+# Fish Config
+#
+# :: Aliases
+# :: Bindings
+# :: Colors
+# :: Exports
+# :: Startup
+
+#
+# Aliases
+#
+
+alias dot="git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv"
+alias fuck="sudo !!"
+alias ls="ls --color=always --group-directories-first"
+alias ll="ls -Ahlv"
+alias tree="tree --dirsfirst -I 'node_modules|plugged|vendor'"
+
+#
+# Bindings
+#
+
+bind -M insert \cf accept-autosuggestion
+bind -M insert \cx accept-autosuggestion execute
+bind -M insert \cp history-search-backward
+bind -M insert \cn history-search-forward
+
+#
+# Colors
+#
+
+set -g fish_color_autosuggestion brblack
+set -g fish_color_cancel red
+set -g fish_color_command green
+set -g fish_color_comment brblack
+set -g fish_color_cwd blue
+set -g fish_color_end yellow
+set -g fish_color_error red
+set -g fish_color_escape cyan
+set -g fish_color_host brblack
+set -g fish_color_match green
+set -g fish_color_normal white
+set -g fish_color_operator yellow
+set -g fish_color_param white
+set -g fish_color_quote yellow
+set -g fish_color_redirection yellow
+set -g fish_color_search_match green
+set -g fish_color_user brblack
+set -g fish_pager_color_completion white
+set -g fish_pager_color_description yellow
+set -g fish_pager_color_prefix green
+set -g fish_pager_color_progress yellow
+set -g fish_pager_color_secondary magenta
+
+#
+# Exports
+#
+
+set -x FZF_COLORS (string join ',' \
+    'bg:-1' \
+    'bg+:-1' \
+    'fg:white' \
+    'fg+:white' \
+    'info:yellow' \
+    'border:-1' \
+    'header:white' \
+    'hl:green' \
+    'hl+:green' \
+    'marker:green' \
+    'pointer:magenta' \
+    'prompt:blue' \
+    'spinner:cyan' \
+)
+
+set -x EDITOR 'nvim'
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_STATE_HOME "$HOME/.local/state"
+set -x FZF_ALT_C_COMMAND "fd --type directory"
+set -x FZF_CTRL_T_COMMAND "rg --files --no-require-git"
+set -x FZF_DEFAULT_COMMAND "fd --type file --follow"
+set -x FZF_DEFAULT_OPTS "--color=$FZF_COLORS"
+set -x PASSWORD_STORE_DIR "$XDG_DATA_HOME/pass"
+
+#
+# Startup
+#
+
+fish_ssh_agent
+
+fish_add_path $HOME/.local/bin
+fish_add_path /usr/local/sbin
+
+if test -e $XDG_CONFIG_HOME/fish/profile.fish
+    source $XDG_CONFIG_HOME/fish/profile.fish
+end
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,25 @@
+function pad_prompt -e fish_preexec -e fish_postexec
+    echo
+end
+
+function fish_prompt
+    set -l directory (dirs)
+    set -l branch (git branch --show-current 2>/dev/null)
+
+    set -l character (
+        switch $fish_bind_mode
+            case default
+                echo '❮'
+
+            case insert
+                echo '❯'
+        end
+    )
+
+    printf '%s %s\n%s ' \
+        (set_color -o blue)"$directory" \
+        (set_color -o black)"$branch" \
+        (set_color -o magenta)"$character"
+
+    set_color normal
+end

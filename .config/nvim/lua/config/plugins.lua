067c2f687ea0d6100b3a3da6d5a45bcac7891f40
Liam Butterworth <contact@liambutterworth.email>
2026-01-24 13:18:21 -0600

    improve lazy bootstrap script

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,17 +1,30 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local repo = 'https://github.com/folke/lazy.nvim.git'
 
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
+if not (vim.uv or vim.loop).fs_stat(path) then
+    local out = vim.fn.system({
         'git',
         'clone',
         '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
         '--branch=stable',
-        lazypath,
+        repo,
+        path,
     })
+
+    if vim.v.shell_error ~= 0 then
+        local messages = {
+            { 'Failed to clone lazy.nvim:\n', "ErrorMsg" },
+            { output, 'WarningMsg' },
+            { '\nPress any key to exit...' },
+        }
+
+        vim.api.nvim_echo(messages, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(path)
 
 require('lazy').setup('plugins', {
     ui = {

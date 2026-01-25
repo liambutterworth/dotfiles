6dfd8199cdf778a4d789e392407639c3d0e00c23
Liam Butterworth <contact@liambutterworth.email>
2026-01-25 16:18:50 -0600

    remove profile.fish and symlinked alacritty config

--- .config/fish/conf.d/profile.fish
+++
@@ -1,6 +0,0 @@
-fish_add_path /usr/local/bin
-
-alias cl2-stage="mycli --login-path=cl2-stage recruiting"
-alias cl2-prod="mycli --login-path=cl2-prod recruiting"
-alias http="http --pretty=all"
-alias jq="jq -C"

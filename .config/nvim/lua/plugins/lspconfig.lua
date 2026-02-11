2c142b8b94ca660722c7c31156292e02bcce9de7
Liam Butterworth <contact@liambutterworth.email>
2026-02-10 18:45:44 -0600

    formatting change

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -42,9 +42,9 @@ return {
             signs = {
                 text = {
                     [vim.diagnostic.severity.ERROR] = '',
-                    [vim.diagnostic.severity.WARN] = '',
-                    [vim.diagnostic.severity.INFO] = '',
                     [vim.diagnostic.severity.HINT] = '',
+                    [vim.diagnostic.severity.INFO] = '',
+                    [vim.diagnostic.severity.WARN] = '',
                 },
             },
         })

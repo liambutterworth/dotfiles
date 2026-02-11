700a01db6433a739f2bb1ae4ca4545e9c1e26eb9
Liam Butterworth <contact@liambutterworth.email>
2026-02-10 18:43:43 -0600

    update tabline

--- .config/nvim/lua/config/tabline.lua
+++ .config/nvim/lua/config/tabline.lua
@@ -9,30 +9,30 @@ function GetTabLine()
         local buffer = vim.api.nvim_win_get_buf(vim.api.nvim_tabpage_get_win(tab))
         local filename =  vim.api.nvim_buf_get_name(buffer)
         local name = vim.fn.fnamemodify(filename, ':p:t')
-        local icon = string.find(filename, 'term://') ~= nil and 'ﲵ' or ''
+        local icon = string.find(filename, 'term://') ~= nil and '' or ''
         local state = tab == current_tab and 'Active' or 'Inactive'
 
         if vim.bo[buffer].readonly then
-            table.insert(tabline, '%#TabLineReadOnly' .. state .. '#')
+            table.insert(tabline, '%#TabLineReadOnly#')
         elseif vim.bo[buffer].modified then
-            table.insert(tabline, '%#TabLineModified' .. state .. '#')
+            table.insert(tabline, '%#TabLineModified#')
         else
-            table.insert(tabline, '%#TabLineUnmodified' .. state .. '#')
+            table.insert(tabline, '%#TabLineUnmodified#')
         end
 
-        table.insert(tabline, ' ' .. icon .. (tab == current_tab and ' ' or ''))
-        table.insert(tabline, '%#TabLine#')
+        table.insert(tabline, icon .. '%#TabLine' .. state .. '#')
         table.insert(tabline, ' ' .. (name ~= '' and name or 'No Name') .. ' ')
 
         if next(vim.diagnostic.get(buffer, { severity = vim.diagnostic.severity.ERROR })) then
-            table.insert(tabline, '%#TabLineHasErrors#')
+            table.insert(tabline, '%#TabLineHasErrors# ')
         elseif next(vim.diagnostic.get(buffer, { severity = vim.diagnostic.severity.INFO })) then
-            table.insert(tabline, '%#TabLineHasInformation#')
+            table.insert(tabline, '%#TabLineHasInformation# ')
+        elseif next(vim.diagnostic.get(buffer, { severity = vim.diagnostic.severity.INFO })) then
+            table.insert(tabline, '%#TabLineHasInformation# ')
         elseif next(vim.diagnostic.get(buffer, { severity = vim.diagnostic.severity.WARN })) then
-            table.insert(tabline, '%#TabLineHasWarnings#')
+            table.insert(tabline, '%#TabLineHasWarnings# ')
         end
 
-        table.insert(tabline, '●')
         table.insert(tabline, '%#TabLine# ')
     end
 

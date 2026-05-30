6bb21559c0e6a8aaac72049fa5363732ea4788ab
Liam Butterworth <contact@liambutterworth.email>
2026-05-30 07:47:18 -0500

    update readme deps; re-add xdg config variables - npm needed it

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,2 +1,6 @@
 set -x EDITOR 'nvim'
 set -x LS_COLORS 'di=34:ln=35:so=32:pi=33:ex=31:ow=34'
+set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_STATE_HOME "$HOME/.local/state"
--- .github/README.md
+++ .github/README.md
@@ -2,7 +2,7 @@
 
 ## Arch
 
-`hyprland hypridle hyprlock hyprpaper hyprshot waybar rofi ghostty`
+`hyprland hypridle hyprlock hyprpaper hyprshot waybar ghostty`
 
 ## Linux
 

63274056abb1a6c78718ab6868a6652dc04de880
Liam Butterworth <contact@liambutterworth.email>
2026-06-02 01:55:40 -0500

    fix mouse binds for hyprland

--- .config/hypr/lua/keybinds.lua
+++ .config/hypr/lua/keybinds.lua
@@ -30,11 +30,11 @@ hl.bind('SUPER + L', hl.dsp.focus({ direction = 'right' }))
 hl.bind('SUPER + SHIFT + L', hl.dsp.window.move({ direction = 'right', group_aware = true }))
 hl.bind('SUPER + N', hl.dsp.layout('rotatesplit 90'))
 hl.bind('SUPER + M', hl.dsp.layout('movetoroot'))
-hl.bind('SUPER + mouse:272', hl.dsp.window.drag(), { mouse = true })
-hl.bind('SUPER + mouse:272', hl.dsp.window.resize(), { mouse = true })
 hl.bind('SUPER + BRACKETLEFT', hl.dsp.layout('splitratio -0.1'))
 hl.bind('SUPER + BRACKETRIGHT', hl.dsp.layout('splitratio +0.1'))
 hl.bind('SUPER + BACKSLASH', hl.dsp.layout('splitratio 1 exact'))
+hl.bind('SUPER + mouse:272', hl.dsp.window.drag(), { mouse = true })
+hl.bind('SUPER + SHIFT + mouse:272', hl.dsp.window.resize(), { mouse = true })
 
 for index = 1, 10 do
     local key = index % 10

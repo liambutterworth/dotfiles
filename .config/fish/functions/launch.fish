16a73d4d4ef984b77ee26eda87ec9b0fe845972a
Liam Butterworth <contact@liambutterworth.email>
2026-06-02 20:45:32 -0500

    simplify launch function

--- .config/fish/functions/launch.fish
+++ .config/fish/functions/launch.fish
@@ -1,9 +1,3 @@
 function launch -d "Launch app"
-    argparse 'q/quit' -- $argv
-
-    command $argv &>/dev/null & disown
-
-    if set -q _flag_quit
-        exit
-    end
+    command $argv &>/dev/null & disown && exit
 end

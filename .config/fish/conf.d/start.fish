c6cb4fd97c55354ef98d0cfe30c3e1671c6abcc6
Liam Butterworth <contact@liambutterworth.email>
2026-06-07 02:14:06 -0500

    minor bugfixes

--- .config/fish/conf.d/start.fish
+++ .config/fish/conf.d/start.fish
@@ -1,5 +1,5 @@
-if command -q start-hyprland; and status is-login
-    if test (tty) = /dev/tty1
-        exec start-hyprland
-    end
-end
+#if command -q start-hyprland; and status is-login
+#    if test (tty) = /dev/tty1
+#        exec start-hyprland
+#    end
+#end
